c06ab6cca8906dd18da5a69e0125ce8def33c689
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-01-27 02:11:21 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,12 @@
+def solution(players, callings):
+    
+    dic = {player: i for i, player in enumerate(players)} # 선수: 등수
+    for call in callings:   # 불리는 선수들 순회
+        idx = dic[call]     # idx는 현재 불린 선수 인덱스
+        
+        dic[call] -= 1      # 불린 선수 인덱스 변경(순위 앞으로)
+        dic[players[idx-1]] += 1     # 앞 선수가 뒤로 추월(순위 뒤로)
+        
+        players[idx], players[idx-1] = players[idx-1], players[idx] # 순서 변경
+        
+    return players

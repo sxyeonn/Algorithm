f75527bf14a8d79dd2f8b0a45de8f6b83ed54ce4
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-02-02 00:50:41 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -0,0 +1,9 @@
+from functools import reduce
+
+def solution(num_list):
+    answer = 0
+    if len(num_list) >= 11:
+        return sum(num_list)
+    else:
+        return reduce(lambda x, y: x * y, num_list)
+    return answer

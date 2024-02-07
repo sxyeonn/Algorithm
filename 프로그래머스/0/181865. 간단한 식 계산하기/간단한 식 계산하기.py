faa642f66e66dd0b84d68c7ee8a7d7362248102a
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-02-08 01:53:06 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,10 @@
+def solution(binomial):
+    answer = 0
+    binomial = binomial.split()
+    if binomial[1] == '+':
+        answer = int(binomial[0]) + int(binomial[2])
+    elif binomial[1] == '-':
+        answer = int(binomial[0]) - int(binomial[2])
+    elif binomial[1] == '*':
+        answer = int(binomial[0]) * int(binomial[2])
+    return answer

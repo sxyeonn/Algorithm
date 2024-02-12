6fbf933f497c50a7ab0b18213e628d5e1ee2762c
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-02-13 01:01:26 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -2,7 +2,6 @@ def solution(arr):
     answer = 0
     for i in range(len(arr)):
         for j in range(len(arr[i])):
-            #print(arr[i][j])
             if arr[i][j] == arr[j][i]:
                 answer = 1
             else:

6a2fa7891cb5293a272d42b53c1a535a2b9c4620
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-02-23 16:44:10 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,10 +1,13 @@
+import math
 def solution(slice, n):
     answer = 0
     
     # n명이 최소 1조각 이상 먹음 => slice * answer
-    
+    print(math.ceil(n/slice))
+    """
     if n%slice == 0:
         answer = n/slice
     else:
         answer = n//slice + 1
-    return answer
+    """
+    return math.ceil(n/slice)

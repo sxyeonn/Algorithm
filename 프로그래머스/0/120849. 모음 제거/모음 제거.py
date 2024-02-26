49fd2058eadcf687c4beb85aeda1210cb117c411
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-02-26 15:55:32 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = ''
+    answer = my_string.replace("a", "")
+    answer = answer.replace("e", "")
+    answer = answer.replace("i", "")
+    answer = answer.replace("o", "")
+    answer = answer.replace("u", "")
+    return answer

5f3bf998bc86e1eab29de2edbc484ed8ea82f411
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-03-06 23:40:21 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,10 +1,13 @@
 def solution(my_string):
+    # 공백을 기준으로 문자열을 나눈다. 
     new_str = my_string.split(' ')
+    # 정확한 수식은 첫 번째로 연산자가 나오지 않으므로 기본 answer은 첫 번째 숫자로 지정한다. 
     answer = int(new_str[0])
     for i in range(len(new_str)):
-        tmp = 0
+        # + 연산자를 만나면, 지금까지의 answer과 연산자 바로 뒤에 올 값을 더한다. 
         if new_str[i] == "+":
             answer = answer + int(new_str[i+1])
+        # - 연산자를 만나면, 지금까지의 answer과 연산자 바로 뒤에 올 값을 뺀다. 
         elif new_str[i] == "-":
             answer = answer - int(new_str[i+1])
     return answer

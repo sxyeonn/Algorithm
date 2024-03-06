3151860b621d0c11ba01c6b0f95d1fee7d5f6560
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-03-06 23:36:15 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,2 +1,10 @@
 def solution(my_string):
-    return eval(my_string)
+    new_str = my_string.split(' ')
+    answer = int(new_str[0])
+    for i in range(len(new_str)):
+        tmp = 0
+        if new_str[i] == "+":
+            answer = answer + int(new_str[i+1])
+        elif new_str[i] == "-":
+            answer = answer - int(new_str[i+1])
+    return answer

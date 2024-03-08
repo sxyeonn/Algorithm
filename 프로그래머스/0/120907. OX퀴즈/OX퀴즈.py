6d5e847cc99fad3973a7c56aabb57bd6a848a4ad
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-03-08 23:14:48 +0900

    [level 0] Title: OX퀴즈, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -2,11 +2,13 @@
 def exp(numbers):
     # 공백을 기준으로 식을 나눔
     expression = numbers.split(" ")
+    # 주어진 수식은 일정한 형태를 보이므로, 연산자는 1번 인덱스에 위치하며, 계산할 값은 각각 0번, 2번 인덱스에 위치함. 
     if expression[1] == "+":
         tmp = int(expression[0]) + int(expression[2])
     else:
         tmp = int(expression[0]) - int(expression[2])
     
+    # 계산한 수식 값이 결과값(수식의 마지막 값)과 일치하면 O, 불일치하면 X 반환
     if tmp == int(expression[-1]):
         return "O"
     else:

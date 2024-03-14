f0bb790966c7965534fc2f6dc7484ddbf6bffb37
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-03-14 15:14:48 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -0,0 +1,13 @@
+import re
+def solution(my_string):
+    answer = 0
+    # 정규표현식 사용
+    m = re.compile('[0-9]+')
+    
+    # 자연수만 담은 리스트 num_list
+    num_list = m.findall(my_string)
+    
+    # num_list의 모든 원소의 합 계산
+    for i in num_list:
+        answer += int(i)
+    return answer

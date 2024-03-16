356867b0d60cc6b3268fa01f32fe102802ae3488
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-03-16 20:32:36 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,15 +1,18 @@
 def solution(spell, dic):
-    answer = 2
+
     new_arr = []
     # spell의 원소가 전부 한 번씩 사용되어야함
+    # spell의 원소 중 하나라도 dic의 각 원소에 없으면 break. 
+    # spell의 원소를 갖고 있는 dic을 new_arr에 추가
     for d in dic:
         for s in spell:
             if s not in d:
                 break
             new_arr.append(d)
-    #print(new_arr)
+            
+    # new_arr에는 spell의 각 원소를 포함하는 dic원소가 모두 들어가있음
     for i in new_arr:
         if new_arr.count(i) == len(spell):
             return 1
             
-    return answer
+    return 2

7f53c5f3de26b6b436228db923847d7823a7609a
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-04-02 19:30:43 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,14 +1,16 @@
 def solution(num, total):
     answer = []
-    
-    # x + x+1 + x+2 + ..
-    # => nx + (1+2+..+n) = total
-    # nx = total-tmp
-    # x = (total-tmp)//n
     tmp = 0
+   	# x + (x+1) + (x+2) + (x+3) ... = total
+    # nx + (1+2+..+n) = total
+    
+    # (1+2+..+n) = tmp
     for i in range(num):
         tmp += i
-    print(tmp, total-tmp, (total-tmp)//num)
+    
+    # 연속된 수 중 처음 수 start_num
     start_num = (total-tmp)//num
+    
+    # start_num부터 1씩 더해 num개의 연속된 수 구하기
     answer = [_ for _ in range(start_num, start_num+num)]
     return answer

2b1b51faedf7ef8134f7c98f557ef1f1f1230edb
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-04-12 20:02:31 +0900

    [level 1] Title: 추억 점수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,7 +1,7 @@
 def solution(name, yearning, photo):
     # photo의 길이만큼 기본 answer 배열 생성
     answer = [0] * len(photo)
-    
+
     # 인물과 추억 점수로 딕셔너리 생성
     score_dic = dict(zip(name, yearning))
     

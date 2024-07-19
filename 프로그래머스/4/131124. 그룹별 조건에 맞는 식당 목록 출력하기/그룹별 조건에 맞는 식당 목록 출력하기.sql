8d00eed671ebe066d5912a99038727b8fba041f7
Kang Suyeon <102640341+SUUYEONN@users.noreply.github.com>
2024-07-19 17:34:22 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,30 +1,7 @@
-/*
-- 리뷰를 가장 많이 작성한 회원의 리뷰 조회
-- 회원 이름, 리뷰 텍스트, 리뷰 작성일이 출력
-- 리뷰 작성일을 기준으로 오름차순, 리뷰 텍스트를 기준으로 오름차순
-*/
--- 고객별 리뷰의 개수를 찾는 쿼리
-WITH find_reviews AS (
-    SELECT MEMBER_ID, REVIEW_TEXT, REVIEW_DATE, count(*) as cnt
-    FROM REST_REVIEW
-    GROUP BY MEMBER_ID
-), 
--- 리뷰를 가장 많이 작성한 회원의 정보를 찾는 쿼리
-find_member AS (
-    SELECT MEMBER_ID, REVIEW_TEXT, REVIEW_DATE
-    FROM find_reviews
-    WHERE cnt IN (SELECT max(cnt) FROM find_reviews)
-), 
-find_member_info AS (
-    SELECT MEMBER_ID, REVIEW_TEXT, REVIEW_DATE
-    FROM REST_REVIEW
-    WHERE MEMBER_ID IN (SELECT MEMBER_ID FROM find_member)
-)
-
-SELECT p.MEMBER_NAME
-        , f.REVIEW_TEXT
-        , DATE_FORMAT(f.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
-FROM find_member_info f 
-INNER JOIN MEMBER_PROFILE p
-ON f.MEMBER_ID = p.MEMBER_ID
-ORDER BY f.REVIEW_DATE, f.REVIEW_TEXT
+SELECT A.MEMBER_NAME,B.REVIEW_TEXT,DATE_FORMAT(B.REVIEW_DATE, "%Y-%m-%d") AS REVIEW_DATE
+from MEMBER_PROFILE A join REST_REVIEW B
+on A.MEMBER_ID = B.MEMBER_ID
+WHERE A.MEMBER_ID = (SELECT MEMBER_ID FROM REST_REVIEW
+GROUP BY MEMBER_ID
+ORDER BY COUNT(*) DESC LIMIT 1)
+order by REVIEW_DATE asc, REVIEW_TEXT

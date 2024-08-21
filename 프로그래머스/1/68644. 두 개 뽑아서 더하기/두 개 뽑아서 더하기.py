3de9def5fc914f9fe3d783e99ab4f1e72819e307
Suyeon Kang <102640341+sxyeonn@users.noreply.github.com>
2024-08-22 00:44:48 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.95 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            tmp = numbers[i] + numbers[j]
+            if tmp not in answer:
+                answer.append(tmp)
+    return sorted(answer)
